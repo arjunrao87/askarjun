8823eef21ac3d244b4bd3c9716d4bbdf0f4aeed3
Arjun Rao <rao.arjun87@gmail.com>
2024-03-20 21:53:30 -0400

    feat: add history

--- samples/langchain/2-rag.py
+++ samples/langchain/2-rag.py
@@ -3,32 +3,12 @@ from langchain_community.document_loaders import WebBaseLoader
 from langchain_community.embeddings import OllamaEmbeddings
 from langchain_community.vectorstores import FAISS
 from langchain_core.prompts import ChatPromptTemplate
-from langchain_core.output_parsers import StrOutputParser
 from langchain_text_splitters import RecursiveCharacterTextSplitter
 from langchain.chains.combine_documents import create_stuff_documents_chain
 from langchain.chains import create_retrieval_chain
 
-# Case 1: Simple chain invocation
-## LLM + Prompt
+# Invoke chain with RAG context
 llm = Ollama(model="mistral")
-output = StrOutputParser()
-prompt = ChatPromptTemplate.from_messages(
-    [
-        (
-            "system",
-            "You are a skilled technical writer.",
-        ),
-        ("human", "{user_input}"),
-    ]
-)
-chain = prompt | llm | output
-
-## Winner winner chicken dinner
-response = chain.invoke({"user_input": "how can langsmith help with testing?"})
-print(":::ROUND 1:::")
-print(response)
-
-# Case 2: Invoke chain with RAG context
 ## Load page content
 loader = WebBaseLoader("https://docs.smith.langchain.com/user_guide")
 docs = loader.load()
@@ -40,7 +20,7 @@ split_documents = text_splitter.split_documents(docs)
 vector_store = FAISS.from_documents(split_documents, embeddings)
 
 ## Prompt construction
-prompt2 = ChatPromptTemplate.from_template(
+prompt = ChatPromptTemplate.from_template(
     """
             Answer the following question only based on the given context
                                                     
@@ -53,7 +33,7 @@ prompt2 = ChatPromptTemplate.from_template(
 )
 
 ## Retrieve context from vector store
-docs_chain = create_stuff_documents_chain(llm, prompt2)
+docs_chain = create_stuff_documents_chain(llm, prompt)
 retriever = vector_store.as_retriever()
 retrieval_chain = create_retrieval_chain(retriever, docs_chain)
 

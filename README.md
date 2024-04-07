d6a005111a16a7b5a0238b330985adb4b0bd94b1
Arjun Rao <rao.arjun87@gmail.com>
2024-04-07 12:01:13 -0400

    feat: adding some bones

--- README.md
+++ README.md
@@ -3,9 +3,9 @@
 ## TODOs
 
 - [x] Build a simple langchain hook to read URL link contents
-- [ ] Langchain crawling of lethain in notebook
-- [ ] Decide between manual vs langchain crawling
-- [ ] Crawl through lethain pages 
+- [x] Langchain crawling of lethain in notebook
+- [x] Decide between manual vs langchain crawling
+- [x] Crawl through lethain pages 
 - [ ] Store contents in vectordb 
 - [ ] Init Ollama & LLM
 - [ ] Ask questions to be answered without memory using vectordb (RAG)
--- main.py
+++ main.py
@@ -1,15 +1,29 @@
-def store_item_to_vectordb():
+from langchain_community.embeddings import OllamaEmbeddings
+from langchain_community.vectorstores import FAISS
+from langchain_text_splitters import RecursiveCharacterTextSplitter
+
+import feedparser
+
+def store_item_to_vectordb(title, link, published, summary):
+    embeddings = OllamaEmbeddings(model="nomic-embed-text")
+    text_splitter = RecursiveCharacterTextSplitter()
+    split_documents = text_splitter.split_documents(docs)
+    vector_store = FAISS.from_documents(split_documents, embeddings)
     return 
 
-def parse_each_item():
-    return
+def parse_each_item(feed):
+    for entry in feed.entries:
+        store_item_to_vectordb(entry.title, entry.link, entry.published, entry.summary)
 
 def parse_rss_xml():
-    
-    return
+    url = "https://lethain.com/feeds.xml"
+    feed = feedparser.parse(url)
+    parse_each_item(feed)
+
 
 def main():
     print("Launching lethain RSS...")
     parse_rss_xml()
+    print("Storage completed.")
 
 main()
